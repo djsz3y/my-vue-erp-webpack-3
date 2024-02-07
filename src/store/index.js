726780bfcc8819c1c814f5986e12ce6d17e921b9
修仙 <294764484@qq.com>
2024-02-07 17:22:56 +0800

    feat: 3-22：登录鉴权解决方案

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore } from 'vuex'
+import getters from './getters'
 import user from './modules/user.js'
+
 export default createStore({
   state: {},
-  getters: {},
+  getters,
   mutations: {},
   actions: {},
   modules: {
