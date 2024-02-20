8de12fe98d17cdb91b57be23aec24169b639e53f
修仙 <294764484@qq.com>
2024-02-20 10:44:07 +0800

    feat: 1.实现 navbar 头像菜单 1.1.获取并展示用户信息（4-03：获取用户基本信息）

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,13 +1,28 @@
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
+import store from '@/store'
 
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
   timeout: 5000
 })
+// 请求拦截器
+service.interceptors.request.use(
+  (config) => {
+    // 在这个位置需要统一的去注入token
+    if (store.getters.token) {
+      // 如果token存在 注入token
+      config.headers.Authorization = `Bearer ${store.getters.token}`
+    }
+    return config // 必须返回配置
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
 // 响应拦截器
 service.interceptors.response.use(
-  response => {
+  (response) => {
     const { success, message, data } = response.data
     //   要根据success的成功与否决定下面的操作
     if (success) {
@@ -18,7 +33,7 @@ service.interceptors.response.use(
       return Promise.reject(new Error(message))
     }
   },
-  error => {
+  (error) => {
     // TODO: 将来处理 token 超时问题
     ElMessage.error(error.message) // 提示错误信息
     return Promise.reject(error)
