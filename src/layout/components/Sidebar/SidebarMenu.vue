af8884266b6a894b58ed26235d96cb2fbac16ac6
修仙 <294764484@qq.com>
2024-02-21 17:21:06 +0800

    feat: 10. 动态菜单项处理方案 3.实现 3.4.生成 menu 菜单（4-14: 业务落地：生成动态 menu 菜单）（已处理无数据报错问题）

--- src/layout/components/Sidebar/SidebarMenu.vue
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -7,7 +7,15 @@
     text-color="#fff"
     active-text-color="#ffd04b"
   >
-    <!-- 子集 menu 菜单 -->
+    <!-- 动态侧边栏 -->
+    <sidebar-item
+      v-for="item in routes"
+      :key="item.path"
+      :route="item"
+    ></sidebar-item>
+
+    <!-- 静态侧边栏：
+    子集 menu 菜单
     <el-submenu index="1">
       <template #title>
         <i class="el-icon-location"></i>
@@ -16,15 +24,17 @@
       <el-menu-item index="1-1">选项1</el-menu-item>
       <el-menu-item index="1-2">选项2</el-menu-item>
     </el-submenu>
-    <!-- 具体菜单项 -->
+
+    具体菜单项
     <el-menu-item index="4">
       <i class="el-icon-setting"></i>
       <template #title>导航四</template>
-    </el-menu-item>
+    </el-menu-item> -->
   </el-menu>
 </template>
 
 <script setup>
+import SidebarItem from './SidebarItem'
 import { computed } from 'vue'
 import { useRouter } from 'vue-router'
 import { filterRouters, generateMenus } from '@/utils/route'
