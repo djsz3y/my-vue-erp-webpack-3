d712fd38c6f3b2258d1d268f81603589b4415261
修仙 <294764484@qq.com>
2024-02-22 15:21:49 +0800

    feat: 5.动态面包屑方案 5.3.计算面包屑结构数据（4-21：业务落地：动态计算面包屑结构数据）

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -11,7 +11,28 @@
 </template>
 
 <script setup>
-import {} from 'vue'
+import { ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+// 生成数组数据
+const breadcrumbData = ref([])
+const getBreadcrumbData = () => {
+  breadcrumbData.value = route.matched.filter(
+    (item) => item.meta && item.meta.title
+  )
+  console.log(breadcrumbData.value)
+}
+// 监听路由变化时触发
+watch(
+  route,
+  () => {
+    getBreadcrumbData()
+  },
+  {
+    immediate: true
+  }
+)
 </script>
 
 <style lang="scss" scoped>
