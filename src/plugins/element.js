9c49df55dbdce60a63a5d0b1990c649bd68caba1
修仙 <294764484@qq.com>
2024-02-23 19:31:22 +0800

    feat: 14. 国际化处理方案 3.国际化 步骤： b.处理对应语言包（两部分） i.导入 el-locale 语言包（5-05：方案落地：element-plus 国际化处理）

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,7 +1,16 @@
 import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
-import locale from 'element-plus/lib/locale/lang/zh-cn'
+// import locale from 'element-plus/lib/locale/lang/zh-cn'
+
+// 1.导入 element 的中文、英文语言包
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import en from 'element-plus/lib/locale/lang/en'
+import store from '@/store'
 
 export default (app) => {
-  app.use(ElementPlus, { locale })
+  app.use(ElementPlus, {
+    // locale
+    // 2.根据当前语言，选择使用哪种语言包
+    locale: store.getters.language === 'en' ? en : zhCn
+  })
 }
