f5039e211255dfbc38bcfb23bfe61eac77c25cd9
修仙 <294764484@qq.com>
2024-02-24 15:54:17 +0800

    feat: 14. 国际化处理方案 3.国际化 步骤： b.处理对应语言包（两部分） ii.创建自定义语言包（5-06：方案落地：自定义语言包国际化处理）

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,14 +1,22 @@
 import { createI18n } from 'vue-i18n'
+// 1.复制 lang 到 i18n
+// 2.导入语言包
+import mZhLocale from './lang/zh'
+import mEnLocale from './lang/en'
 
 const messages = {
   en: {
     msg: {
-      test: 'hello world'
+      // test: 'hello world'
+
+      ...mEnLocale // 3.注册到语言包
     }
   },
   zh: {
     msg: {
-      test: '你好世界'
+      // test: '你好世界'
+
+      ...mZhLocale // 3.注册到语言包
     }
   }
 } // 【1. 创建 messages 数据源】
