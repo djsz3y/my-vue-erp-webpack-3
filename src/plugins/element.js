3e7514d0874ea7dcb7e111d7ee8d516c1b686ef0
修仙 <294764484@qq.com>
2024-02-24 19:05:39 +0800

    feat: 14. 国际化处理方案 4.更新 a.更新：关于 element-plus 国际化问题更新

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -2,15 +2,16 @@ import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
 // import locale from 'element-plus/lib/locale/lang/zh-cn'
 
-// 1.导入 element 的中文、英文语言包
-import zhCn from 'element-plus/es/locale/lang/zh-cn'
-import en from 'element-plus/lib/locale/lang/en'
-import store from '@/store'
+// // 1.导入 element 的中文、英文语言包
+// import zhCn from 'element-plus/es/locale/lang/zh-cn'
+// import en from 'element-plus/lib/locale/lang/en'
+// import store from '@/store'
 
 export default (app) => {
-  app.use(ElementPlus, {
-    // locale
-    // 2.根据当前语言，选择使用哪种语言包
-    locale: store.getters.language === 'en' ? en : zhCn
-  })
+  // app.use(ElementPlus, {
+  //   // locale
+  //   // 2.根据当前语言，选择使用哪种语言包
+  //   locale: store.getters.language === 'en' ? en : zhCn
+  // })
+  app.use(ElementPlus) // element-plus 已经提供了 国际化的处理方案
 }
