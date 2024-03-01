1e89a6084fc2ab7e8616b89df64bdba61251df37
修仙 <294764484@qq.com>
2024-03-01 17:06:23 +0800

    feat: 15. 动态主题处理方案（动态换肤功能） 3.实现 b.根据新值修改 element-plus  主题色（5-15/16/17） iv.小问题：在刷新页面后，新主题会失效（5-17：方案落地：element-plus 新主题的立即生效）

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,13 @@
 
 <script setup>
 import { useStore } from 'vuex'
-// import { generateNewStyle, writeNewStyle } from '@/utils/theme'
+import { generateNewStyle, writeNewStyle } from '@/utils/theme'
 // import { watchSwitchLang } from '@/utils/i18n'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import en from 'element-plus/lib/locale/lang/en'
 
 const store = useStore()
-// generateNewStyle(store.getters.mainColor).then(newStyleText => {
-
-// })
+generateNewStyle(store.getters.mainColor).then((newStyleText) => {
+  writeNewStyle(newStyleText)
+})
 </script>
