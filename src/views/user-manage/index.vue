73e36e10c42a2c5afe267c7e6eb38a012d208f52
修仙 <294764484@qq.com>
2024-03-23 14:09:46 +0800

    feat: 24. RBAC 的权限分控体系 2.辅助业务 c.为用户分配角色 d.为角色指定权限

--- src/views/user-manage/index.vue
+++ src/views/user-manage/index.vue
@@ -1,9 +1,34 @@
 <template>
-  <div class="">用户管理</div>
+  <div class="">
+    <roles-dialog
+      v-model="roleDialogVisible"
+      :userId="selectUserId"
+      @updateRole="getListData"
+    ></roles-dialog>
+  </div>
 </template>
 
 <script setup>
-import {} from 'vue'
+import { ref, watch } from 'vue'
+import RolesDialog from './components/roles.vue'
+
+/**
+ * 查看角色的点击事件
+ */
+const roleDialogVisible = ref(false)
+
+const selectUserId = ref('')
+const onShowRoleClick = (row) => {
+  console.log(row)
+  roleDialogVisible.value = true
+  // selectUserId.value = row._id
+}
+onShowRoleClick()
+
+// 保证每次打开重新获取用户角色数据
+watch(roleDialogVisible, (val) => {
+  if (!val) selectUserId.value = ''
+})
 </script>
 
 <style lang="scss" scoped></style>
