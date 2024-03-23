1686d8fe1191087fc7bd2d46d6aa1736e178d4ce
修仙 <294764484@qq.com>
2024-03-23 18:03:24 +0800

    feat: §7 权限架构（处理用户权限）-Excel&Zip&用户管理 3.全局属性，处理时间展示 问题

--- src/main.js
+++ src/main.js
@@ -10,9 +10,13 @@ import installIcons from '@/icons'
 import './permission.js'
 // i18n （PS：导入放到 APP.vue 导入之前，因为后面我们会在 app.vue 中使用国际化内容）
 import i18n from '@/i18n'
+// filter
+import installFilter from '@/filters'
 
 const app = createApp(App)
 installElementPlus(app)
 installIcons(app)
+installFilter(app)
+
 app.use(store).use(router).mount('#app')
 app.use(i18n)
